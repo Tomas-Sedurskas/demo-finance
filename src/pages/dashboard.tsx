cbaef6d55f353ea1b86439b4584bbdc732a4ec90
Tomas-Sedurskas <tomas.sedurskas@gmail.com>
2024-02-11 00:06:19 +0200

    Add partial dashboard layout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { Card } from "@/components/card";
+import { Button } from "@/components/ui/button";
 
 export const Dashboard = () => {
   return (
@@ -8,10 +9,44 @@ export const Dashboard = () => {
           <Card key={key} />
         ))}
       </div>
-      <div className="flex flex-col w-full">
-        <div> Add Overview Here</div>
+      <div className="flex flex-col w-full gap-2">
+        <div className="flex w-full gap-2">
+          <div className="flex flex-col gap-2">
+            <div className="border rounded-md p-6 flex flex-col gap-6">
+              <div>
+                <span>Total Balance</span>
+                <div className="text-5xl">9,294€</div>
+              </div>
+              <div className="flex gap-2 text-lg">
+                <span>****</span>
+                <span>****</span>
+                <span>****</span>
+                <span>1234</span>
+              </div>
+              <div className="flex gap-2">
+                <Button className="w-full">Transfer</Button>
+                <Button className="w-full">Request</Button>
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <div className="border rounded-md p-6">
+                <span>Total Income</span>
+                <div className="text-3xl">9,294€</div>
+              </div>
+              <div className="border rounded-md p-6">
+                <span>Total Expenses</span>
+                <div className="text-3xl">9,294€</div>
+              </div>
+            </div>
+          </div>
 
-        <div>Add Transactions Here</div>
+          <div className="flex flex-col w-full border rounded-md p-6">
+            Add Spending Limit Here
+          </div>
+        </div>
+        <div className="flex flex-col w-full border rounded-md p-6">
+          Add Transactions Here
+        </div>
       </div>
     </div>
   );
